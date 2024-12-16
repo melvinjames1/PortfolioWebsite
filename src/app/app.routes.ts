709d5d7c8f48d74df48cff77fff80b6511d34ebf
Melvin James <123574144+melvinjames1@users.noreply.github.com>
2024-12-16 16:30:18 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
-import { BlogComponent } from './blog/blog.component';
+
 
 
 export const routes: Routes = [
