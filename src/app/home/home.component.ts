5efc08c1f6ba73c9a51f87fb4f59e31ef7e746f8
Melvin James <melvin.mj576@gmail.com>
2025-07-06 00:04:56 +0530

    added new resume

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -84,6 +84,10 @@ export class HomeComponent implements AfterViewInit {
       title: "Frontend",
       skills: [
         {
+          name: "Angular",
+          image: "assets/angular-icon.svg"
+        },
+        {
           name: "React Js",
           image:
             "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
@@ -191,7 +195,7 @@ export class HomeComponent implements AfterViewInit {
       skills: [
         {
           name: "Canva",
-          image: "https://upload.wikimedia.org/wikipedia/commons/0/08/Canva_icon_2021.svg",
+          image: "assets/canva-icon.svg",
         },
         {
           name: "Figma",
